cdcf3eea395b31e7f0ae826ca15dd7ea638e3823
SirGoodEnough <james.chaloupka.1@gmail.com>
2022-01-19 22:38:12 -0600

    fix batteries

---
+++ python_scripts/battery_clear.py
@@ -0,0 +1,15 @@
+
+sensors = [
+    "BatteryLowMailbox",
+    "BatteryLowBedroom",
+    "BatteryLowGarageC",
+    "BatteryLowSideDoorI",
+    "BatteryLowBathroom",
+    "BatteryLowGarageSide",
+    "BatteryLowFrontDoorI",
+    "BatteryLowGarageO",
+    "BatteryLowBackDoorI"
+    ]
+for x in sensors:
+    service_data = {'topic':'rf433/{}'.format(x), 'payload':'OFF', 'qos':1, 'retain':'true'}
+    hass.services.call('mqtt', 'publish', service_data, False)
