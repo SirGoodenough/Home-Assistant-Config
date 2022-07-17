1a1bef4af1d19e046a930d3002e49519c9cee958
SirGoodEnough <james.chaloupka.1@gmail.com>
2022-07-17 00:48:20 -0500

    fun with gitignore

---
+++ python_scripts/battery_clear.py
@@ -0,0 +1,16 @@
+
+sensors = [
+    "BatteryLowMailbox",
+    "BatteryLowBedroom",
+    "BatteryLowGarageC",
+    "BatteryLowSideDoorI",
+    "BatteryLowBathroom",
+    "BatteryLowGarageSide",
+    "BatteryLowFrontDoorI",
+    "BatteryLowGarageO",
+    "BatteryLowBackDoorI",
+    "BasementSmoke"
+    ]
+for x in sensors:
+    service_data = {'topic':'rf433/{}'.format(x), 'payload':'OFF', 'qos':1, 'retain':'true'}
+    hass.services.call('mqtt', 'publish', service_data, False)
